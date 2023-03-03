efc8de6af3ae36a6a3974aa9d1d2e04c0982a2a3
Ebele Okemba <okemba50@gmail.com>
2023-03-03 14:49:45 +0000

    create a function in the app file that returns the content of the pagecontainer through importation olf the file

--- src/components/PagesContainer.js
+++ src/components/PagesContainer.js
@@ -11,15 +11,21 @@ function PagesContainer() {
 return (
   <Router>
     <div>
+
+      {/* rendering the header to all the pages with the project */}
       <Header />
       {/* Wrap Route elements in a Routes component */}
       <Routes>
         {/* Define routes using the Route component to render different page components at different paths */}
         {/* Define a default route that will render the Home component */}
         <Route path="/" element={<Home />} />
+        {/* definie a default route that will render the About Component */}
         <Route path="about" element={<About />} />
+        {/* definie a default route that will render the Project Component */}
         <Route path="project" element={<Project />} />
+        {/* definie a default route that will render the Project Gallery Component */}
         <Route path="projectgallery" element={<ProjectGallery />} />
+        {/* definie a default route that will render the Contact Component */}
         <Route path="contact/*" element={<Contact />} />
       </Routes>
     </div>
