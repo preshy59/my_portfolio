1d51ffdf3a46244e96261b1b90d8dd72c4a229d7
Ebele Okemba <okemba50@gmail.com>
2023-03-04 14:45:20 +0000

    added code to the project gallery

---
+++ src/components/pages/ProjectGallery.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ProjectGallery() {
+  return (
+    <div>
+      <h1 className="text-center">hello</h1>
+      <p>
+       What kind of dog will you like to search for?
+      </p>
+    </div>
+  );
+}
+
+export default ProjectGallery;
