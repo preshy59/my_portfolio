dcfc060e281bb5845c5191b5225cbf8b4340e2d7
Ebele Okemba <okemba50@gmail.com>
2023-03-07 14:55:28 +0000

    modify the index.js in home and added its corresponding css

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -4,22 +4,19 @@ import "./style.css";
 
 function Home() {
     return (
-        <div className="card mb-3" id="body">
-            <div className="row g-0">
-                <div className="col-md-10">
-                    <div className="card-body" id="message">
-                        <h2 className="card-title">HI !!!, my name is</h2>
-                        <h3 className="card-title"> Ebele Preious Okemba</h3>
-                        <p className="card-text"> I'm a Front-End Developer that has interest in different fields, but most especially in Artificial
-                            Intelligence and Machine Learning. I'm currently sharpening my skills as a Front-End Developer and mastering the act of
-                            visualization as a Data Analyst.Currently based in London and open for freelance and collaborations. </p>
-                    </div>
-                </div>
-                <div className="col-md-2">
-                    <img src= {require('../../../assets/images/headshot.png')} className="img-fluid rounded-start" alt="headshot"></img>
-                </div>
-            </div>
+
+        <div className="jumbotron home">
+            <div className="content" id="section">
+                <h1 className="display-4">HI !!!, I'm Ebele Okemba!</h1>
+                <h2 className="display-4">I build things for Web</h2>
+                <p className="lead">I'm a Front-End Developer that has interest in different fields, but most especially in Software Development, Artificial
+                    Intelligence and Machine Learning. I'm currently sharpening my skills as a Front-End Developer and mastering the act of
+                    visualization as a Data Analyst. Currently based in London and open for freelance and collaborations. </p>
+
+             </div>
+
         </div>
+
 
     );
 
