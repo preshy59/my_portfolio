90a179b12f818ec85001c5a60d77de205ebfd39d
Ebele Okemba <okemba50@gmail.com>
2023-03-07 22:45:55 +0000

    modify the pagecontainer file by adding the corresponding router path

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,36 +6,36 @@ function Header(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light" id="mainNav">
             <a className="navbar-brand images navHead" href="#home" onClick={() => props.handlePageChange("Home")}>
-                    <img src={require('../../assets/images/logo.png')} alt="logo using initial" id="logo"></img>
-                </a>
-                 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink
-                                to="/"
-                                end
-                                className={({ isActive }) =>
-                                    isActive ? 'nav-link active' : 'nav-link'
-                                }
-                            >
-                                Home
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink
-                                to="/about"
-                                className={({ isActive }) =>
-                                    isActive ? 'nav-link active' : 'nav-link'
-                                }
-                            >
-                                About
-                            </NavLink>
-                        </li>
-                        {/* <li className="nav-item">
+                <img src={require('../../assets/images/logo.png')} alt="logo using initial" id="logo"></img>
+            </a>
+
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <NavLink
+                            to="/my_portfolio/"
+                            end
+                            className={({ isActive }) =>
+                                isActive ? 'nav-link active' : 'nav-link'
+                            }
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink
+                            to="/my_portfolio/about"
+                            className={({ isActive }) =>
+                                isActive ? 'nav-link active' : 'nav-link'
+                            }
+                        >
+                            About
+                        </NavLink>
+                    </li>
+                    {/* <li className="nav-item">
                             <NavLink
                                 to="/project"
                                 className={({ isActive }) =>
@@ -46,40 +46,34 @@ function Header(props) {
                             </NavLink>
                         </li> */}
 
-                        <li className="nav-item">
-                            <NavLink
-                                to="/projectgallery"
-                                className={({ isActive }) =>
-                                    isActive ? 'nav-link active' : 'nav-link'
-                                }
-                            >
-                                Project Gallery
-                            </NavLink>
-                        </li>
+                    <li className="nav-item">
+                        <NavLink
+                            to="/my_portfolio/projectgallery"
+                            className={({ isActive }) =>
+                                isActive ? 'nav-link active' : 'nav-link'
+                            }
+                        >
+                            Project Gallery
+                        </NavLink>
+                    </li>
 
-                        <li className="nav-item">
-                            <NavLink
-                                to="contact"
-                                className={({ isActive }) =>
-                                    isActive ? 'nav-link active' : 'nav-link'
-                                }
-                            >
-                                Contact
-                            </NavLink>
-                        </li>
+                    <li className="nav-item">
+                        <NavLink
+                            to="/my_portfolio/contact"
+                            className={({ isActive }) =>
+                                isActive ? 'nav-link active' : 'nav-link'
+                            }
+                        >
+                            Contact
+                        </NavLink>
+                    </li>
 
-                        <li className="nav-item">
-                            
-                               
-                                
-                            
-                        </li>
-                    </ul>
-                    <div className="resume">
+                </ul>
+                <div className="resume">
                     <a href="/Resume.pdf" download>Resume</a>
-                    </div>
                 </div>
-               </nav>
+            </div>
+        </nav>
     );
 }
 
--- src/components/PagesContainer.js
+++ src/components/PagesContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./Header";
 import Contact from "./pages/Contact";
-import Project from "./pages/Project";
+//import Project from "./pages/Project";
 import ProjectGallery from "./pages/ProjectGallery";
 import About from "./pages/About"
 import Home from "./pages/Home";
@@ -16,18 +16,18 @@ return (
       {/* rendering the header to all the pages with the project */}
       <Header />
       {/* Wrap Route elements in a Routes component */}
-      <Routes>
+      <Routes basename = "my_portfolio">
         {/* Define routes using the Route component to render different page components at different paths */}
         {/* Define a default route that will render the Home component */}
-        <Route path="/" element={<Home />} />
+        <Route path="/my_portfolio/" element={<Home />} />
         {/* definie a default route that will render the About Component */}
-        <Route path="/about" element={<About />} />
+        <Route path="/my_portfolio/about" element={<About />} />
         {/* definie a default route that will render the Project Component */}
-        <Route path="/project" element={<Project />} />
+        {/* <Route path="/my_portfolio/project" element={<Project />} /> */}
         {/* definie a default route that will render the Project Gallery Component */}
-        <Route path="/projectgallery" element={<ProjectGallery />} />
+        <Route path="/my_portfolio/projectgallery" element={<ProjectGallery />} />
         {/* definie a default route that will render the Contact Component */}
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/my_portfolio/contact" element={<Contact />} />
       </Routes>
     </div>
   </Router>
